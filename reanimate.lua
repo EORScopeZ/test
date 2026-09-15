6baab0bb96caf434d0f03cee6c930d907ace5fce
Biscuit <98918076+EORScopeZ@users.noreply.github.com>
2026-09-14 21:46:27 -0400

    Update reanimate.lua

--- reanimate.lua
+++ reanimate.lua
@@ -9694,6 +9694,6 @@ end
 
 _G.onyxAPI = _api or _G.onyxAPI
 
-print(string.format("[Reanimate] loaded in %.4fs", os.clock() - _reanimLoadStart))
+print(string.format("reanim loaded in %.4fs", os.clock() - _reanimLoadStart))
 
 return _G.onyxAPI
